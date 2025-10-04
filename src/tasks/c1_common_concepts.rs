329e72821d01c09c04b81d28ecc9d631ecedd23d
Alexander Kozhevnikov <kozhevnikov.av@phystech.edu>
2025-10-04 18:07:09 +0300

    Added task c1

--- src/tasks/c1_common_concepts.rs
+++ src/tasks/c1_common_concepts.rs
@@ -9,7 +9,11 @@
 // it to 10, and prints both values.
 #[allow(dead_code)]
 pub fn simple_mutability() {
-    unimplemented!()
+    let mut number: i32 = 5;
+    println!("{number}");
+
+    number = 10;
+    println!("{number}");
 }
 
 // DATA TYPES
@@ -20,7 +24,12 @@ pub fn simple_mutability() {
 // them.
 #[allow(dead_code)]
 pub fn simple_data_types() {
-    unimplemented!()
+    let i32_var: i32 = 1;
+    let f64_var: f64 = 1.0;
+    let bool_var: bool = true;
+    let char_var: char = '1';
+
+    println!("i32: {i32_var}\nf64: {f64_var}\nbool: {bool_var}\nchar_var: {char_var}")
 }
 
 // FUNCTIONS
@@ -30,11 +39,17 @@ pub fn simple_data_types() {
 // Write a function `square` that takes a `u32` integer and returns its square as `u32`.
 
 // IMPLEMENT HERE:
+pub fn square(number: u32) -> u32 {
+    number * number
+}
 
 // ----- 4 --------------------------------------
 // Write a recursive function `factorial` that computes the factorial of a number (n!) as `u32`.
 
 // IMPLEMENT HERE:
+pub fn factorial(number: u32) -> u32 {
+    if number == 0 { 1 } else { number * factorial(number - 1) }
+}
 
 // CONTROL FLOW
 // ================================================================================================
@@ -43,12 +58,25 @@ pub fn simple_data_types() {
 // Write a program that prints whether a provided signed integer number is positive, negative, or
 // zero using `if` statement.
 pub fn sign_checker(number: i32) -> &'static str {
-    unimplemented!()
+    if number > 0 {
+        "positive"
+    } else if number < 0 {
+        "negative"
+    } else {
+        "zero"
+    }
 }
 
 // ----- 6 --------------------------------------
 // Write a program that finds the largest number in an array of 5 integers using a for or while
 // loop.
 pub fn find_biggest_number(some_array: [u32; 5]) -> u32 {
-    unimplemented!()
+    let mut max_value: u32 = 0;
+    for value in some_array {
+        if value > max_value {
+            max_value = value;
+        }
+    }
+
+    max_value
 }
