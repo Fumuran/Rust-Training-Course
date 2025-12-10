ef723d245ebe72007cd69ae8ab5029db4877240d
Ivanov Mikhail <ivanov.mikhail@phystech.edu>
2025-12-10 12:27:18 +0300

    Task is done, all tests are passed.

--- src/tasks/c1_common_concepts.rs
+++ src/tasks/c1_common_concepts.rs
@@ -9,7 +9,10 @@
 // it to 10, and prints both values.
 #[allow(dead_code)]
 pub fn simple_mutability() {
-    unimplemented!()
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 10;
+    println!("The value of x is: {x}");
 }
 
 // DATA TYPES
@@ -20,7 +23,17 @@ pub fn simple_mutability() {
 // them.
 #[allow(dead_code)]
 pub fn simple_data_types() {
-    unimplemented!()
+  let i32_var: i32 = 4;
+  println!("The value of i32_var is: {i32_var}");
+
+  let f64_var: f64 = 4.2;
+  println!("The value of f64_var is: {f64_var}");
+
+  let bool_var: bool = true;
+  println!("The value of bool_var is: {bool_var}");
+
+  let char_var: char = 'a';
+  println!("The value of char_val is: {char_var}");
 }
 
 // FUNCTIONS
@@ -29,13 +42,18 @@ pub fn simple_data_types() {
 // ----- 3 --------------------------------------
 // Write a function `square` that takes a `u32` integer and returns its square as `u32`.
 
-// IMPLEMENT HERE:
-
+pub fn square(number: u32) -> u32 {
+  number * number
+}
 // ----- 4 --------------------------------------
 // Write a recursive function `factorial` that computes the factorial of a number (n!) as `u32`.
 
-// IMPLEMENT HERE:
-
+pub fn factorial(number: u32) -> u32 {
+  if number == 0 {
+    return 1;
+  }
+  number * factorial(number - 1)
+}
 // CONTROL FLOW
 // ================================================================================================
 
@@ -43,12 +61,22 @@ pub fn simple_data_types() {
 // Write a program that prints whether a provided signed integer number is positive, negative, or
 // zero using `if` statement.
 pub fn sign_checker(number: i32) -> &'static str {
-    unimplemented!()
+  if number > 0 {
+    "positive"
+  } else if number == 0 {
+    "zero"
+  } else {
+    "negative"
+  }
 }
 
 // ----- 6 --------------------------------------
 // Write a program that finds the largest number in an array of 5 integers using a for or while
 // loop.
 pub fn find_biggest_number(some_array: [u32; 5]) -> u32 {
-    unimplemented!()
+  let mut max_number: u32 = 0;
+  for number in some_array {
+    max_number = if number > max_number { number } else {max_number};
+  }
+  max_number
 }
